a6bf00cfc6c557110e5e9f850076441cb5487e66
Ogo43 <emehaogochukwu@gmail.com>
2022-07-07 19:05:50 +0100

    sumMix

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,5 +1,11 @@
 function sumMix(arr) {
-   
- }
-
+    let sum = 0;
+    // declare your loop
+   for(let i=0; i<arr.length; i++) {
+     console.log(arr[i])
+     sum = sum + Number(arr[i]);
+   }
+     // add the numbers and the string on the array
+     console.log(sum)
+       } 
   module.exports = sumMix
