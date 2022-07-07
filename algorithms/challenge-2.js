31baecf187573f2aacaaace921fc70c25443b264
Ogo43 <emehaogochukwu@gmail.com>
2022-07-07 18:27:42 +0100

    secondLargest

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,5 +1,16 @@
 function filterArray(arr) {
- 
+ //i move go through the arry
+ let result=[]
+ for( let i = 0; i < arr.length; i++){ 
+  
+   //how to know if it is a nan and not string with number
+   if (typeof arr[i] != 'string') {
+  result.push(arr[i])  
+}
+  
+  }
+
+return result
 }
 
 module.exports = filterArray
