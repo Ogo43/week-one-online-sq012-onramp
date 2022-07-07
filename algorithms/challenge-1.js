2c2de91d855db4ee93c0783524da80c2554a5411
Ogo43 <emehaogochukwu@gmail.com>
2022-07-07 18:20:56 +0100

    secondlargest

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,25 @@
 function secondLargest(arr) {
-  
+    //if and empty arr return 0
+    if(arr.length===0){
+        return 0
+      }
+     //if one item return that item 
+        if(arr.length ===1){
+        return arr[0]
+      }
+      //actual code
+      let firstMax=Math.max(...arr);
+     
+      //remove firstmax from the arry
+        for( let i = 0; i < arr.length; i++){ 
+        
+            if ( arr[i] === firstMax) { 
+        
+                arr.splice(i, 1); 
+            }
+        
+        }
+     
+      return Math.max(...arr);
 }
 module.exports = secondLargest
