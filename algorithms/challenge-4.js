778064e4ed65118cc40f45c5533344f21b0e5e54
Ogo43 <emehaogochukwu@gmail.com>
2022-07-08 19:33:18 +0100

    sumTwoSmallestNums

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,5 +1,19 @@
 function sumTwoSmallestNums(arr) {
+    // remove smallest numbers
+    var min1 = Math.min(...arr);
+      console.log({min1})
+   for( let i=0; i < arr.length; i++) {
+
+      if(arr[i] === min1){
+       
+        arr.splice(i, 1); 
+      };
     
+     // sum= sum + arr[i]
+   }
+   var min2= Math.min(...arr);  
+   console.log({min2})
+  return min1 + min2
  
  }
   module.exports =sumTwoSmallestNums
